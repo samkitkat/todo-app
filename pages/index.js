3fdddb1daaf5391a2a4c4c7f52853b0d9327bcd2
JuiceMitApfelnDrin <juicemitapfelndrin@gmail.com>
2023-05-19 13:39:28 +0200

    remove hydration error in index.js due to there not being a consistent way of rendering time, add an .env.example, adjust .gitignore to not ignore the .env.example

--- pages/index.js
+++ pages/index.js
@@ -1,45 +1,49 @@
-import * as React from 'react';
-// import Head from 'next/head'
-// import Layout, { siteTitle } from '../components/layout'
-import styles from '../styles/page.module.css'
-import Todo from '../components/todo.js'
+import * as React from "react";
+
+import { Toast, useToast } from "@trycourier/react-toast";
 
 import { CourierProvider } from "@trycourier/react-provider";
 import { Inbox } from "@trycourier/react-inbox";
-import { Toast, useToast } from "@trycourier/react-toast";
+import Todo from "../components/todo.js";
+// import Head from 'next/head'
+// import Layout, { siteTitle } from '../components/layout'
+import styles from "../styles/page.module.css";
 
 // import Link from 'next/link'
 // import Date from '../components/date'
 
 export default function Home() {
-
-
   return (
-
     <div>
-    <main className={styles.main}>
+      <main className={styles.main}>
+        <CourierProvider
+          userId={"todo-app-test"}
+          clientKey={"ZWQxNWFkYjMtM2M1NC00MGM4LWFkODEtNDczNzUwMWNlZjcw"}
+        >
+          <Inbox />
+          <Toast />
+        </CourierProvider>
 
-      <CourierProvider userId={"todo-app-test"} clientKey={"ZWQxNWFkYjMtM2M1NC00MGM4LWFkODEtNDczNzUwMWNlZjcw"}>
-        <Inbox />
-        <Toast />
-      </CourierProvider>
+        <div className={styles.description}>
+          <p>
+            {`${new Date().toLocaleString("en-US", {
+              month: "long",
+              day: "2-digit",
+            })} Todo's`}
+          </p>
+        </div>
 
-      <div className={styles.description}>
-        <p>
-          {new Date().toLocaleString("en-US", { month: "long", day: '2-digit' })} Todo's
-        </p>
-      </div>
+        <div className={styles.timer}>
+          <p>
+            {new Date().toLocaleTimeString("en-US", {
+              hour: "2-digit",
+              minute: "2-digit",
+            })}
+          </p>
+        </div>
 
-      <div className={styles.timer}>
-        <p>
-          {new Date().toLocaleTimeString([], { hour: "2-digit", minute: "2-digit" })}
-        </p>
-      </div>
-
-      <Todo />
-
-    </main>
+        <Todo />
+      </main>
     </div>
-
-  )
+  );
 }
